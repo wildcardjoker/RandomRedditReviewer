b329d992d78fd8682f99889184f8653d5277e0ad
wildcardjoker <the.pctechnician+github@gmail.com>
2016-08-12 21:31:52 +0800

    Fix assembly versions.

--- SubReddit/Properties/AssemblyInfo.cs
+++ SubReddit/Properties/AssemblyInfo.cs
@@ -1,14 +1,24 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿#region Information
+
+// RandomRedditReviewer: SubReddit
+// Created: 2016-07-31
+// Modified: 2016-08-12 9:31 PM
+#endregion
+
+#region Using Directives
+using System.Reflection;
 using System.Runtime.InteropServices;
+
+#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("SubReddit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Wild Card Joker")]
 [assembly: AssemblyProduct("SubReddit")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -17,9 +27,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("8bf72329-9ba4-46c9-9bc6-bc571696c5b9")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +44,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("1.0.*")]
