d048031500b095e7d56112c939dd1d2386df0661
wildcardjoker <the.pctechnician+github@gmail.com>
2016-08-12 20:48:41 +0800

    Fix assembly version. Add metadata.

--- RandomRedditReviewer/Properties/AssemblyInfo.cs
+++ RandomRedditReviewer/Properties/AssemblyInfo.cs
@@ -1,16 +1,25 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
+﻿#region Information
+
+// RandomRedditReviewer: RandomRedditReviewer
+// Created: 2016-07-31
+// Modified: 2016-08-12 8:26 PM
+#endregion
+
+#region Using Directives
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
+
+#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("RandomRedditReviewer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Find Random Subreddits")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Wild Card Joker")]
 [assembly: AssemblyProduct("RandomRedditReviewer")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -19,6 +28,7 @@ using System.Windows;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 //In order to begin building localizable applications, set 
@@ -30,16 +40,14 @@ using System.Windows;
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                                     //(used if a resource is not found in the page, 
-                                     // or application resource dictionaries)
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page, 
-                                              // app, or any theme specific resource dictionaries)
-)]
-
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+    )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -51,5 +59,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("1.0.*")]
